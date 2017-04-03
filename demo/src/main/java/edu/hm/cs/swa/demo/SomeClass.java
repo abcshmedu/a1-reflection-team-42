1b016760089eadd8915aa58a69864a57fb91039a
thomas murschall <thomas.murschall@hm.edu>
2017-04-03 14:55:11 +0200

    Removed App class and App tests, removed main in SomeClass and added RendererTest

--- demo/src/main/java/edu/hm/cs/swa/demo/SomeClass.java
+++ demo/src/main/java/edu/hm/cs/swa/demo/SomeClass.java
@@ -2,19 +2,24 @@ package edu.hm.cs.swa.demo;
 
 import java.util.Date;
 
-public class SomeClass 
-{
-	@RenderMe private int foo;
-	@RenderMe(with="edu.hm.renderer.ArrayRenderer") int[] array = {1, 2, 3, };
-	@RenderMe private Date date = new Date(123456789);
-	
-	public SomeClass(int foo) {
-		this.foo = foo;
-	}
-	
-	public static void main (String... strings){
-		SomeClass sc = new SomeClass(5);
-		Renderer rend = new Renderer(sc);
-		rend.render();
-	} 
+/**
+ * 
+ * @author Markus Krahl, Thomas Murschall
+ *
+ */
+public class SomeClass {
+    @RenderMe private int foo;
+    
+    @RenderMe(with = "edu.hm.renderer.ArrayRenderer") private final int[] array = {1, 2, 3, };
+    private final int time = 123456789;
+    @RenderMe private Date date = new Date(time);
+    
+    /**
+     * 
+     * @param foo 
+     */
+    public SomeClass(int foo) {
+        this.foo = foo;
+    }
+   
 }
---
+++ demo/src/test/java/edu/hm/cs/swa/demo/RendererTest.java
@@ -0,0 +1,39 @@
+package edu.hm.cs.swa.demo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * 
+ * @author Thomas Murschall
+ *
+ */
+public class RendererTest {
+   
+    private SomeClass toRender;
+    private Renderer renderer;
+    
+    /**
+     * Setup for the tests.
+     * 
+     * Initialization of renderer and SomeClass instance.
+     */
+    @Before
+    public void setUp() {
+        final int foo = 5;
+        toRender = new SomeClass(foo);
+        renderer = new Renderer(toRender);
+    }
+
+    /**
+     * 
+     * @throws Exception 
+     */
+    @Test
+    public void testRendering() throws Exception {
+        assertEquals("Instance of edu.hm.SomeClass:\n" 
+                + "foo (Type int): 5\narray (Type int[]) [1, 2, 3, ]\ndate (Type java.util.Date): Fri Jan 02 11:17:36 CET 1970\n", renderer.render());
+    }
+}
